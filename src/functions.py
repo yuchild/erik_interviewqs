4de33ca43ae96c33ff3efd7e37bb4b376c9630d1
yuchild <yuchild@gmail.com>
2021-11-10 13:53:42 -0800

    updated 2021-11-09

---
+++ src/functions.py
@@ -0,0 +1,12 @@
+def add_two(a, b, c):
+    return a + b + 5 * c
+
+import pandas as pd
+import numpy as np
+
+def return_df():
+    return pd.read_pickle('./data/teleco_user_data.pkl')
+
+def return_transfromed_df():
+    ...
+
